a0130984b9870553236d42c0025f451a3033e919
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-15 22:38:31 -0400

    Elimina import innecesario

--- app/src/main/java/edu/ucne/exampledatastore/MainActivity.kt
+++ app/src/main/java/edu/ucne/exampledatastore/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
